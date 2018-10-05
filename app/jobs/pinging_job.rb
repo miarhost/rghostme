7be066634ff9c3cdda909ec553a82b81d4badcff
MariaTikhonova <ritz.rkraft@gmail.com>
2018-10-05 18:08:54 +0300

    pinging job

---
+++ app/jobs/pinging_job.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'timeout'
+
+class PingJob < ApplicationJob	
+  self.queue_adapter = :async
+  queue_as :urgent
+
+  def ping
+
+   response = Net::HTTP.get('https://rghostme.herokuapp.com', '/index.html')
+    
+	Net::HTTP.start('https://rghostme.herokuapp.com', 80, open_timeout: 5) do |http|
+      http.request response 
+    end
+
+  end
+
+end
